8e439e1249b92056e06ae785436488d62d7b6164
capinowo <alyasafina507@gmail.com>
2024-12-27 11:21:53 +0700

    yoink

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -1,9 +1,9 @@
 <template>
   <aside class="sidebar">
     <ul>
-      <li><a href="/menu1">Menu 1</a></li>
-      <li><a href="/menu2">Menu 2</a></li>
-      <li><a href="/menu3">Menu 3</a></li>
+      <li><a href="/client">Daftar Client</a></li>
+      <li><a href="/message">Message</a></li>
+      <li><a href="/log">Log</a></li>
     </ul>
   </aside>
 </template>
