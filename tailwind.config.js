bf9f9a27a5c3bda19b963b12491332054e033b26
capinowo <alyasafina507@gmail.com>
2024-12-27 14:23:29 +0700

    re

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    './components/**/*.{vue,js}',
-    './layouts/**/*.vue',
-    './pages/**/*.vue',
-    './composables/**/*.{js,ts}',
-    './plugins/**/*.{js,ts}',
-    './app.vue', // Nuxt 3 main entry file
-  ],
+export default {
+  content: [],
   theme: {
     extend: {},
   },
   plugins: [],
 }
+
